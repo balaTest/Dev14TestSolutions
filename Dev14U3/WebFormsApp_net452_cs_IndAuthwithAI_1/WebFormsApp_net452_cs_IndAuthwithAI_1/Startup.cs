a0cab5561a5537ebfb25d9d1696c8c8bab9c77da
fwtlaba <fwtlaba@BALACHaVM180706>
2018-07-06 04:17:56 -0700

    [WebFormsApp_net452_cs_IndAuthwithAI_1] Created solution using Dev14 U3

---
+++ Dev14U3/WebFormsApp_net452_cs_IndAuthwithAI_1/WebFormsApp_net452_cs_IndAuthwithAI_1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsApp_net452_cs_IndAuthwithAI_1.Startup))]
+namespace WebFormsApp_net452_cs_IndAuthwithAI_1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
